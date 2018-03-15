f39d2673a4f26fac383bcfba19b2b18880f9d571
KGKallasmaa <karl.gustav1789@gmail.com>
2018-03-15 13:57:56 +0200

    -

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,19 +1,24 @@
-<div class="col-lg-8 col-lg-offset-2">
+<link rel = 'stylesheet' href="css/bootstrap.min.css" type="text/css">
+<div class="col-md-4">
+    <h1>Log in</h1>
     <?php echo form_open('index.php/Auth/login'); ?>
     <form action="" method="post" autocomplete="on" target="_top">
         <div class="form-group">
             <label for = "email">Email: </label>
             <input class="forms-control" name="email"type="text" placeholder="email">
         </div>
+        <br>
         <div class="form-group">
             <label for = "password">Password: </label>
             <input class="forms-control" name="password" type="password" placeholder="password">
         </div>
+        <br>
         <div class="text-center">
             <button class="btn btn-primary" name="login" type="submit" value="login">Login</button>
         </div>
     </form>
 </div>
+
 
 <!--
 
