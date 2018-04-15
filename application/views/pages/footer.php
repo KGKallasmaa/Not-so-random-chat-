d6e778f0b4baa38748c55ae34b00d6548f8b35e3
Techgi <Kasemaa1@gmail.com>
2018-04-15 23:57:30 +0300

    Etapp 4

--- application/views/pages/footer.php
+++ application/views/pages/footer.php
@@ -1,5 +1,5 @@
-
-	<div class="footer">
+<!DOCTYPE html>
+<div class="footer">
 		<a href='<?php echo base_url();?>index.php/Pages/about'><?php echo lang("about"); ?></a>
 		<a href="<?php echo base_url();?>index.php/Pages/tos"><?php echo lang("terms"); ?></a>
 		<a href="<?php echo base_url();?>index.php/Pages/stat"><?php echo lang("statistic"); ?></a>
