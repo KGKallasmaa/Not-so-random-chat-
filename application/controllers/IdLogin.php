21ff9e22d4f3c061db2ca35700540f98c9269aad
Techgi <Kasemaa1@gmail.com>
2018-04-29 22:06:04 +0300

    some id card login stuff, can't get SSL to work

---
+++ application/controllers/IdLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+class IdLogin extends CI_controller{
+	public function index(){
+		
+		if (isset ($_SERVER["SSL_CLIENT_S_DN_CN"])){
+			$idCardInfo = $_SERVER["SSL_CLIENT_S_DN_CN"];
+			$pos = strrpos($idCardInfo, ',');
+			$idCode = substr($idCardInfo, $pos+1);
+			echo $idCode;
+			$email = null;
+			if (isset($_SERVER["SSL_CLIENT_SAN_Email_0"])){
+				$email = $_SERVER["SSL_CLIENT_SAN_Email_0"];
+			}
+			$user = $this->user_model->get_user_id($idCode);
+			if ($user === null){
+				$this->user_model->insert_IDuser($email, $idCode);
+				$user = $this->user_model->get_user_id($idCode);
+			}
+			$_SESSION['logged_in'] = true;
+			$_SESSION['id'] = $user['id'];
+			redirect(site_url('chat'));
+		}
+		else {
+			echo lang('id_fail');
+		}
+	}
+}
