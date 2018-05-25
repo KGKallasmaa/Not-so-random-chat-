4a20d2e6276eb728740df46389a6102491294bc3
Paul <pungpaul2@gmail.com>
2018-05-26 01:33:25 +0300

    chat.js

--- js/chat.js
+++ js/chat.js
@@ -14,16 +14,54 @@ setInterval(function()
 
 
           //  var obj = JSON.parse(data);
-            var obj =$.parseJSON(data) ;
-            console.log(typeof obj);
+            var obj =JSON.parse(data) ;
+            console.log(obj.length);
+			var i;
+			var text ="";
+			for (i = 0; i < obj.length; i++) { 
+				text += obj[i].sender+": "+obj[i].message + "<br>";
+			}
 
 
 
         //    console.log(JSON.stringify(data));  // returns ["person", "age"]
 
-            document.getElementById("chat_log").innerHTML = obj.sender + ", " + obj.message;
+            document.getElementById("chat_log").innerHTML = text;
            // document.getElementById("chat_log").innerHTML = data.sender + data.message;
           //  document.getElementById("chat_log").innerHTML = data;
         }
     });
-}, 10000);//time in milliseconds
+}, 5000);//time in milliseconds
+
+$(document).ready(function(){
+	    $.ajax({
+        type:"post",
+        url:"get_conversation",
+        datatype:'JSON',
+        success:function(data)
+        {
+
+
+
+        //    console.log(JSON.parse(data));
+
+
+          //  var obj = JSON.parse(data);
+            var obj =JSON.parse(data) ;
+            console.log(obj.length);
+			var i;
+			var text ="";
+			for (i = 0; i < obj.length; i++) { 
+				text +="<div class = 'chat_line'>" + obj[i].sender+": "+obj[i].message + "<br>";
+			}
+
+
+
+        //    console.log(JSON.stringify(data));  // returns ["person", "age"]
+
+            document.getElementById("chat_log").innerHTML = text;
+           // document.getElementById("chat_log").innerHTML = data.sender + data.message;
+          //  document.getElementById("chat_log").innerHTML = data;
+        }
+    });
+});
